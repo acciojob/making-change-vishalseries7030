b0006bf79c821c1c928ddda9748a19bdfa5c4451
vishal payyawar <139535850+vishalseries7030@users.noreply.github.com>
2025-07-29 16:14:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const makeChange = (c) => {
-  // your name here
+  c = parseInt(c); // Ensure input is treated as an integer
+
+  const change = {};
+
+  change.q = Math.floor(c / 25);
+  c %= 25;
+
+  change.d = Math.floor(c / 10);
+  c %= 10;
+
+  change.n = Math.floor(c / 5);
+  c %= 5;
+
+  change.p = c;
+
+  return change;
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
+
